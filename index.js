b9b553667dde2d3534d2e98e7d8e85dc07c5502c
unibreakfast <unibreakfast1@gmail.com>
2020-10-27 20:58:15 +0200

    upgradeRequire

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 upgradeRequire()
 
-setInterval(() => require('./module.js', true)(), 1500)
+const fn = require('./module.js', true)
+
+setInterval(fn, 1500)
 
 
 function upgradeRequire() {
   const originalRequire = require
-  const reloadingModules = []
+  const reloadingWrappers = {}
 
   require = function require(module, watchToReload) {
-    if (watchToReload && !reloadingModules.includes(module)) {
-      reloadingModules.push(module)
+    if (watchToReload) {
+      if (module in reloadingWrappers) {
+        return reloadingWrappers[module]
+      } else {
+        originalRequire("fs").watch(
+          originalRequire("path").resolve(module),
+          () => delete require.cache[require.resolve(module)]
+        )
 
-      originalRequire("fs").watch(
-        originalRequire("path").resolve(module),
-        () => delete require.cache[require.resolve(module)]
-      )
+        reloadingWrappers[module] = function (...args) {
+          return originalRequire(module).apply(this, args)
+        }
+
+        return Object.assign(reloadingWrappers[module], originalRequire(module))
+      }
     }
 
     return originalRequire(module)
   }
 
   Object.assign(require, originalRequire)
+
+  upgradeRequire = () => {}
 }
